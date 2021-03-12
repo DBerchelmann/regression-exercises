ce2c0350eb8bd09cf118a32a5b80a9a6bed380ae
David Berchelmann <david.a.berchelmann@gmail.com>
2021-03-12 16:18:00 -0600

    updated .py file

--- prepare.py
+++ prepare.py
@@ -11,9 +11,21 @@ from wrangle import split_data, wrangle_telco
    
 def get_scaled():
     
+    '''
+    This function is used to scale data using the min/max scale function.
+    Would need to update if wanting to use standard, robust, quantile or other type.
+    
+    '''
+    
+    # call data in from the wrangle function
+    
     train, validate, test = wrangle_telco()
     
+    # make our scaler below
+    
     scaler = sklearn.preprocessing.RobustScaler()
+    
+    # be sure to fit the data to the TRAIN dataset, nothing else
     
     scaler.fit(train)
     
@@ -22,6 +34,7 @@ def get_scaled():
     test_scaled = scaler.transform(test)
     
     # turn the numpy arrays into dataframes
+    
     train_scaled = pd.DataFrame(train_scaled, columns=train.columns)
     validate_scaled = pd.DataFrame(validate_scaled, columns=train.columns)
     test_scaled = pd.DataFrame(test_scaled, columns=train.columns)
